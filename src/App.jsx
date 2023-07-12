e995b3e950fca2a2d1bab740c7466045d5f5dfee
Arthur40480 <arthurgibert@free.fr>
2023-07-12 11:07:26 +0200

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
